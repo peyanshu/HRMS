e51ad7cddd39afe6e0e0e28494d44fd973b19a20
Peyanshu Verma <130502409+peyanshu@users.noreply.github.com>
2025-09-28 00:09:17 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -26,7 +26,7 @@ const PORT = process.env.PORT || 5000;
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
+  app.listen(PORT, "0.0.0.0", () => {      
       console.log(`Server running on http://localhost:${PORT}`);
     });
   })
